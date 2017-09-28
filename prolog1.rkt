daa5180d83fed2c16340402fa0abe8b81d7cf084
Luis Marcelo Rosso <lrosso@garbarino.com>
2017-09-28 13:25:56 -0300

    Section 11.2 ported to Racket

--- prolog1.rkt
+++ prolog1.rkt
@@ -4,115 +4,131 @@
 
 ;;;; File prolog1.lisp: First version of the prolog interpreter (11.2).
 
-(requires "unify")
+#lang racket
+
+(require "unify.rkt"
+         "auxfns.rkt")
+
+(module+ test
+  (require rackunit))
 
 ;; Clauses are represented as (head . body) cons cells
-(defun clause-head (clause) (first clause))
-(defun clause-body (clause) (rest clause))
+(define (clause-head clause)
+  (first clause))
+(define (clause-body clause)
+  (rest clause))
 
-;; Clauses are stored on the predicate's plist
-(defun get-clauses (pred) (get pred 'clauses))
-(defun predicate (relation) (first relation))
+;; clauses are stored on the predicate's plist
+(define (get-clauses pred)
+  (hash-ref db-predicates pred))
+(define (predicate relation)
+  (first relation))
 
-(defvar *db-predicates* nil
-  "A list of all predicates stored in the database.")
+;; A list of all predicates stored in the database
+(define db-predicates (make-hash))
 
-(defmacro <- (&rest clause)
-  "Add a clause to the data base."
-  `(add-clause ',clause))
+;; Add a clause to the data base
+(define-syntax-rule (<- fact1 ...)
+  (add-clause (list 'fact1 ...)))
 
-(defun add-clause (clause)
-  "Add a clause to the data base, indexed by head's predicate."
-  ;; The predicate must be a non-variable symbol.
+;; Add a clause to the data base, indexed by head's predicate
+(define (add-clause clause)
   (let ((pred (predicate (clause-head clause))))
-    (assert (and (symbolp pred) (not (variable-p pred))))
-    (pushnew pred *db-predicates*)
-    (setf (get pred 'clauses)
-          (nconc (get-clauses pred) (list clause)))
+    ;; The predicate must be a non-variable symbol
+    (when (and (symbol? pred) (not (variable? pred)))
+      (if (hash-has-key? db-predicates pred)
+          (hash-update! db-predicates pred
+                        (lambda (value)
+                          (append value (list clause))))
+          (hash-set! db-predicates pred (list clause))))
     pred))
 
-(defun clear-db ()
-  "Remove all clauses (for all predicates) from the data base."
-  (mapc #'clear-predicate *db-predicates*))
+;; Remove all clauses (for all predicates) from the data base
+(define (clear-db)
+  (hash-clear! db-predicates))
 
-(defun clear-predicate (predicate)
-  "Remove the clauses for a single predicate."
-  (setf (get predicate 'clauses) nil))
+;; Remove the clauses for a single predicate
+(define (clear-predicate predicate)
+  (hash-remove! db-predicates predicate))
 
-(defun prove (goal bindings)
-  "Return a list of possible solutions to goal."  
-  (format t "prove - goal: ~a~%" goal)
-  (format t "prove - bindings: ~a~%" bindings)
-  (mapcan #'(lambda (clause)
-              (let ((new-clause (rename-variables clause)))
-                (prove-all (clause-body new-clause)
-                           (unify goal (clause-head new-clause) bindings))))
-          (get-clauses (predicate goal))))
+;; Return a 1ist of possible solutions to goal
+(define (prove goal bindings)
+  (my-flatten (filter (lambda (elem)
+                        (not (null? elem)))
+                      (map (lambda (clause)
+                             (let ((new-clause (rename-variables clause)))
+                               (prove-all (clause-body new-clause)
+                                          (unify goal (clause-head new-clause) bindings))))
+                           (get-clauses (predicate goal))))))
 
-(defun prove-all (goals bindings)
-  "Return a list of solutions to the conjunction of goals."
-  (format t "prove-all - goals: ~a~%" goals)
-  (format t "prove-all - bindings: ~a~%" bindings)
-  (cond ((eq bindings fail) fail)
-        ((null goals) (list bindings))
-        (t (mapcan #'(lambda (goal1-solution)
-                       (prove-all (rest goals) goal1-solution))
+;; Return a list of solutions to the conjunction of goals
+(define (prove-all goals bindings)
+  (cond ((equal? bindings fail) fail)
+        ((null? goals) bindings)
+        (else (map (lambda (goal1-solution)
+                     (prove-all (rest goals) goal1-solution))
                    (prove (first goals) bindings)))))
 
-(defun rename-variables (x)
-  "Replace all variables in x with new ones."
-  (format t "rename-variables - x: ~a~%" x)
-  (sublis (mapcar #'(lambda (var) (cons var (gensym (string var))))
-                  (variables-in x))
+;; Replace all variables in x with new ones
+(define (rename-variables x)
+  (sublis (map (lambda (var) (cons var (gensym (symbol->string var))))
+               (variables-in x))
           x))
 
-(defun unique-find-anywhere-if (predicate tree
-                                &optional found-so-far)
-  "Return a list of leaves of tree satisfying predicate,
-  with duplicates removed."
-  (if (atom tree)
-      (if (funcall predicate tree)
-          (adjoin tree found-so-far)
-          found-so-far)
+;; Return a list of leaves of tree satisfying predicate, with duplicates removed
+(define (unique-find-anywhere-if predicate tree (found-so-far '()))
+  (if (cons? tree)
       (unique-find-anywhere-if
-        predicate
-        (first tree)
-        (unique-find-anywhere-if predicate (rest tree)
-                                 found-so-far))))
+       predicate
+       (first tree)
+       (unique-find-anywhere-if predicate (rest tree) found-so-far))
+      (if (apply predicate (list tree))
+          (adjoin tree found-so-far)
+          found-so-far)))
 
-(defun find-anywhere-if (predicate tree)
-  "Does predicate apply to any atom in the tree?"  
-  (if (atom tree)
-      (funcall predicate tree)
-      (or (find-anywhere-if predicate (first tree))
-          (find-anywhere-if predicate (rest tree)))))
+(module+ test
+  (check-equal? '(?who) (unique-find-anywhere-if variable? '((likes Sandy ?who))))
+  (check-equal? null (unique-find-anywhere-if variable? '((likes Kim Robin))))
+  (check-equal? '(?x) (unique-find-anywhere-if variable? '((likes Kim ?x) (likes ?x Lee) (likes ?x Kim))))
+  (check-equal? '(?x) (unique-find-anywhere-if variable? '((likes ?x ?x))))
+  (check-equal? '(?x) (unique-find-anywhere-if variable? '((likes Sandy ?x) (likes ?x cats)))))
 
-(defmacro ?- (&rest goals) `(top-level-prove ',goals))
+(define-syntax-rule (?- goal1 ...)
+  (top-level-prove (list 'goal1 ...)))
 
-(defun top-level-prove (goals)
-  "Prove the goals, and print variables readably."
+;; Prove the goals, and print variables readably
+(define (top-level-prove goals)
   (show-prolog-solutions
-    (variables-in goals)
-    (prove-all goals no-bindings)))
+   (variables-in goals)
+   (prove-all goals no-bindings)))
 
-(defun show-prolog-solutions (vars solutions)
-  "Print the variables in each of the solutions."  
-  (if (null solutions)
-      (format t "~&No.")
-      (mapc #'(lambda (solution) (show-prolog-vars vars solution))
-            solutions))
+;; Print the variables in each of the solutions
+(define (show-prolog-solutions vars solutions)
+  (if (null? solutions)
+      (format "~%No.")
+      (map (lambda (solution) (show-prolog-vars vars solution))
+           solutions))
   (values))
 
-(defun show-prolog-vars (vars bindings)
-  "Print each variable with its binding."
-  (if (null vars)
-      (format t "~&Yes")
-      (dolist (var vars)
-        (format t "~&~a = ~a" var
-                (subst-bindings bindings var))))
-  (princ ";"))
+;; Print each variable with its binding
+(define (show-prolog-vars vars bindings)
+  (if (null? vars)
+      (displayln "Yes")
+      (for ((var vars))
+                (displayln
+                 (string-append
+                  (symbol->string var) " = "
+                  (symbol->string (subst-bindings bindings var)) ";")))))
 
-(defun variables-in (exp)
-  "Return a list of all the variables in EXP."
-  (unique-find-anywhere-if #'variable-p exp))
+(module+ test
+  (check-equal? 'Lee (subst-bindings '((?who . Lee)) '?who))
+  (check-equal? 'Kim (subst-bindings '((?who . Kim)) '?who))
+  (check-equal? 'Robin (subst-bindings '((?x71156 . Robin) (?who . ?x71156)) '?who))
+  (check-equal? 'Sandy (subst-bindings '((?x71160 . cats) (?x71157 . cats) (?x71156 . Sandy) (?who . ?x71156)) '?who))
+  (check-equal? 'cats (subst-bindings '((?x71169 . cats) (?x71156 . ?x71169) (?who . ?x71156)) '?who))
+  (check-equal? 'Sandy (subst-bindings '((?who . Sandy) (?x71171 . Sandy)) '?who)))
+
+;; Return a list of all the variables in EXP
+(define (variables-in exp)
+  (unique-find-anywhere-if variable? exp))
 
--- unify.rkt
+++ unify.rkt
@@ -4,62 +4,110 @@
 
 ;;;; File unify.lisp: Unification functions
 
-(requires "patmatch")
+#lang racket
 
-(defparameter *occurs-check* t "Should we do the occurs check?")
+(require "auxfns.rkt")
 
-(defun unify (x y &optional (bindings no-bindings))
-  "See if x and y match with given bindings."
-  (format t "unify - x: ~a~%" x)
-  (format t "unify - y: ~a~%" y)
-  (format t "unify - bindings: ~a~%" bindings)
-  (cond ((eq bindings fail) fail)
-        ((eql x y) bindings)
-        ((variable-p x) (unify-variable x y bindings))
-        ((variable-p y) (unify-variable y x bindings))
-        ((and (consp x) (consp y))
-         (unify (rest x) (rest y) 
+(provide unify
+         subst-bindings)
+
+(module+ test
+  (require rackunit))
+
+(define occurs-check? #t)
+
+;; Auxiliar function for unit test
+(define (set-occurs-check value)
+  (set! occurs-check? value))
+
+;; See if x and y match with given bindings
+(define (unify x y [bindings no-bindings])
+  (cond ((equal? bindings fail) fail)
+        ((equal? x y) bindings)
+        ((variable? x) (unify-variable x y bindings))
+        ((variable? y) (unify-variable y x bindings))
+        ((and (cons? x) (cons? y))
+         (unify (rest x) (rest y)
                 (unify (first x) (first y) bindings)))
-        (t fail)))
+        (else fail)))
 
-(defun unify-variable (var x bindings)
-  "Unify var with x, using (and maybe extending) bindings."
-  (format t "unify-variable - var: ~a~%" var)
-  (format t "unify-variable - x: ~a~%" x)
-  (format t "unify-variable - bindings: ~a~%" bindings)
+(module+ test
+  (check-equal? '((?x . ?y)) (unify '(f ?x) '(f ?y)))
+  (check-equal? '((?y . 0) (?x . ?y) (?a . ?x)) (unify '(?a + ?a = 0) '(?x + ?y = ?y)))
+  (check-equal? '((?y . 1) (?x . 2)) (unify '(?x + 1) '(2 + ?y)))
+  (check-equal? '((?x . ?y)) (unify '?x '?y))
+  (check-equal? '((?x . ?y)) (unify '(?x ?x) '(?y ?y)))
+  (check-equal? '((?x . ?y)) (unify '(?x ?y) '(?y ?x)))
+  (check-equal? '((?y . a) (?x . ?y)) (unify '(?x ?y a) '(?y ?x ?x)))
+  (check-equal? '((?y . a) (?x . ?y)) (unify '(?x ?y a) '(?y ?x ?x)))
+  (check-equal? '((?y . a) (?x . ?y)) (unify '(?x ?y a) '(?y ?x ?x)))
+  (check-equal? '() (unify '?x '(f ?x)))
+  (check-equal? '() (unify '(?x ?y) '((f ?y) (f ?x))))
+  (check-equal? '() (unify '(?x ?y ?z) '((?y ?z) (?x ?z) (?x ?y))))
+  (check-equal? '((#t . #t)) (unify 'a 'a))
+  )
+
+(module+ test
+  (test-begin
+   (set-occurs-check #f)
+   (check-equal? '((?x f ?x)) (unify '?x '(f ?x)))
+   (check-equal? '((?y f ?x) (?x f ?y)) (unify '(?x ?y) '((f ?y) (f ?x))))
+   (check-equal? '((?z ?x ?y) (?y ?x ?z) (?x ?y ?z)) (unify '(?x ?y ?z) '((?y ?z) (?x ?z) (?x ?y))))
+   (set-occurs-check #t)))
+
+;; Unify var with x, using (and maybe extending) bindings
+(define (unify-variable var x bindings)
   (cond ((get-binding var bindings)
          (unify (lookup var bindings) x bindings))
-        ((and (variable-p x) (get-binding x bindings))
+        ((and (variable? x) (get-binding x bindings))
          (unify var (lookup x bindings) bindings))
-        ((and *occurs-check* (occurs-check var x bindings))
+        ((and occurs-check? (occurs-check var x bindings))
          fail)
-        (t (extend-bindings var x bindings))))
+        (else (extend-bindings var x bindings))))
 
-(defun occurs-check (var x bindings)
-  "Does var occur anywhere inside x?"
-  (cond ((eq var x) t)
-        ((and (variable-p x) (get-binding x bindings))
+;; Does var occur anywhere 'inside x?
+(define (occurs-check var x bindings)
+  (cond ((equal? var x) #t)
+        ((and (variable? x) (get-binding x bindings))
          (occurs-check var (lookup x bindings) bindings))
-        ((consp x) (or (occurs-check var (first x) bindings)
+        ((cons? x) (or (occurs-check var (first x) bindings)
                        (occurs-check var (rest x) bindings)))
-        (t nil)))
+        (else #f)))
 
 ;;; ==============================
 
-(defun subst-bindings (bindings x)
-  "Substitute the value of variables in bindings into x,
-  taking recursively bound variables into account."
-  (cond ((eq bindings fail) fail)
-        ((eq bindings no-bindings) x)
-        ((and (variable-p x) (get-binding x bindings))
+;; Substitute the value of variables in bindings into x, taking recursively bound variables into account
+(define (subst-bindings bindings x)
+  (cond ((equal? bindings fail) fail)
+        ((equal? bindings no-bindings) x)
+        ((and (variable? x) (get-binding x bindings))
          (subst-bindings bindings (lookup x bindings)))
-        ((atom x) x)
-        (t (reuse-cons (subst-bindings bindings (car x))
-                       (subst-bindings bindings (cdr x))
-                       x))))
+        ((not (cons? x)) x)
+        (else (reuse-cons (subst-bindings bindings (car x))
+                          (subst-bindings bindings (cdr x))
+                          x))))
+
+(module+ test
+  (check-equal? 'Lee (subst-bindings '((?who . Lee)) '?who))
+  (check-equal? 'Kim (subst-bindings '((?who . Kim)) '?who))
+  (check-equal? 'Robin (subst-bindings '((?x71156 . Robin) (?who . ?x71156)) '?who))
+  (check-equal? 'Sandy (subst-bindings '((?x71160 . cats) (?x71157 . cats) (?x71156 . Sandy) (?who . ?x71156)) '?who))
+  (check-equal? 'cats (subst-bindings '((?x71169 . cats) (?x71156 . ?x71169) (?who . ?x71156)) '?who))
+  (check-equal? 'Sandy (subst-bindings '((?who . Sandy) (?x71171 . Sandy)) '?who)))
 
 ;;; ==============================
 
-(defun unifier (x y)
- "Return something that unifies with both x and y (or fail)."
- (subst-bindings (unify x y) x))
+;; Return something that unifies with both x and y (or fail)
+(define (unifier x y)
+  (subst-bindings (unify x y) x))
+
+(module+ test
+  (check-equal? '(0 + 0 = 0) (unifier '(?a + ?a = 0) '(?x + ?y = ?y)))
+  (check-equal? '(2 + 2 = 2) (unifier '(?a + ?a = 2) '(?x + ?y = ?y)))
+  (check-equal? '(a a a) (unifier '(?x ?y a) '(?y ?x ?x)))
+  (check-equal? '((?a * 5 ^ 2) + (4 * 5) + 3) (unifier '((?a * ?x ^ 2) + (?b * ?x) + ?c)
+                                                       '(?z + (4 * 5) + 3)))
+  (check-equal? '(a a a) (unifier '(?x ?y a) '(?y ?x ?x)))
+  (check-equal? '(a a a) (unifier '(?x ?y a) '(?y ?x ?x)))
+  (check-equal? '(a a a) (unifier '(?x ?y a) '(?y ?x ?x)))
+  )
